8b1dbdb86eb8a768726850af813100c0f26ab45c
RajvinderYogi <rajvinderyogi@yahoo.com>
2018-03-17 15:43:41 -0400

    setup navigation, installed mongoose, setup DB on mlab and setup db connection

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//added references
+let mongoose =require ('mongoose');
+
+let config = require ('./config/globals');
+
+var index = require('./controllers/index');
+var users = require('./controllers/users');
 
 var app = express();
 
@@ -24,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+//DB Connection
+mongoose.connect(config.db);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config/globals.js
@@ -0,0 +1,3 @@
+module.exports={
+    db:'mongodb://rajvinderyogi:27591*Mani@ds117489.mlab.com:17489/cellphonemarketplace'
+};
