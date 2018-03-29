57286a8f579aaf41d4ad78fe8737d892de94b6e8
RajvinderYogi <rajvinderyogi@yahoo.com>
2018-03-29 00:28:46 -0400

    CRUD is working for samsung and apple

--- controllers/samsungs.js
+++ controllers/samsungs.js
@@ -1,8 +1,88 @@
 let express = require('express');
 let router = express.Router();
+let Samsung = require('../models/samsung');
 
-    router.get('/', (req, res, next)=> {
-    res.render('samsungs/index', {title: 'Samsung Phones'})
+//GET: index
+router.get('/', (req, res, next) => {
+    //get data from db
+    Samsung.find((err,samsungs)=>{
+        if (err){
+            console.log(err);
+        }
+        else {
+            res.render('samsungs/index', {title:"Samsung Phones", samsungs:samsungs});
+        }
+    });
 });
 
+//GET: addPhone
+router.get('/addPhone', (req, res, next)=>{
+    res.render('samsungs/addPhone', { title:'Add new Phone'});
+});
+
+//POST: apples/addPhone
+router.post('/addPhone', (req, res, next)=>{
+    //save the new phone here
+    Samsung.create({
+        model: req.body.model,
+        color: req.body.color,
+        usage:req.body.usage,
+        price:req.body.price
+    }, (err, samsungs)=>{
+        if (err){
+            console.log(err);
+        }
+        else {
+            res.redirect('/samsungs')
+        }
+    });
+});
+//GET: Delete
+router.get('/delete/:_id', (req, res, next) =>{
+    let _id= req.params._id;
+
+    Samsung.remove({_id: _id}, (err) =>{
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.redirect('/samsungs');
+        }
+    });
+});
+
+//GET: Edit
+router.get('/edit/:_id', (req, res, next)=>{
+    let _id= req.params._id;
+    Samsung.findById(_id, (err, samsungs)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render('samsungs/edit', {
+                title:'Update the ad',
+                samsungs: samsungs,
+            });
+        }
+    });
+});
+//POST: Samsung/edit
+router.post('/edit/:_id', (req, res, next)=>{
+    let _id = req.params._id;
+
+    Samsung.update({_id:_id},
+        {$set:{
+                model: req.body.model,
+                color: req.body.color,
+                usage: req.body.usage,
+                price: req.body.price
+            }}, null, (err)=>{
+            if (err){
+                console.log(err);
+            }
+            else {
+                res.redirect('/samsungs')
+            }
+        });
+});
     module.exports = router;
---
+++ models/samsung.js
@@ -0,0 +1,23 @@
+//mongoose reference
+let mongoose = require ('mongoose');
+
+//Apple phone schema
+let samsungSchema = new mongoose.Schema({
+    model:{
+        type: String,
+        required:"Please enter the model of phone"
+    },
+    color:{
+        type: String
+    },
+    usage:{
+        type: String,
+        required:"Please enter either phone is new or used"
+    },
+    price:{
+        type: Number,
+        required:"You ust enter the price of this phone"
+    }
+});
+//make public
+module.exports = mongoose.model('Samsung', samsungSchema);
