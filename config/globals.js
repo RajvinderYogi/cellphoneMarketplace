2996d5738db476226989ba04b89d2b8a9edbaff5
RajvinderYogi <rajvinderyogi@yahoo.com>
2018-04-02 22:28:17 -0400

    resolved callback url issue

--- config/globals.js
+++ config/globals.js
@@ -3,20 +3,20 @@ module.exports= {
     google:{
         googleClientId:'352903993244-03i8k5d1q4jpcpfuum637ja5d4u83jsi.apps.googleusercontent.com',
         googleClientSecret:'FRcREPzRRQPAuM9Y4L5OPXzI',
-        googleCallbackUrl:'http://localhost:3000/google/callback'
-        // googleCallbackUrl:'https://cellphonemarketplace.herokuapp.com/google/callback'
+        // googleCallbackUrl:'http://localhost:3000/google/callback'
+        googleCallbackUrl:'https://cellphonemarketplace.herokuapp.com/google/callback'
     },
     linkedin:{
         linkedinClientId:'78pk21txaovdq8',
         linkedinClientSecret:'6AawCs1Pnt8iVgeU',
-        linkedinCallbackUrl:'http://localhost:3000/linkedin/callback'
-        // linkedinCallbackUrl:'https://cellphonemarketplace.herokuapp.com/linkedin/callback'
+        // linkedinCallbackUrl:'http://localhost:3000/linkedin/callback'
+        linkedinCallbackUrl:'https://cellphonemarketplace.herokuapp.com/linkedin/callback'
     },
     instagram: {
         instaclientID: '791fd739980d497a8842e11d8cbb0815',
         instaclientSecret: 'ac32526320734f129841a45ba84ed5c6',
-        instacallbackURL: 'http://localhost:3000/instagram/callback',
-        // instaCallbackUrl:'https://cellphonemarketplace.herokuapp.com/instagram/callback'
+        // instacallbackURL: 'http://localhost:3000/instagram/callback',
+        instaCallbackUrl:'https://cellphonemarketplace.herokuapp.com/insagram/callback'
 
     }
 };
